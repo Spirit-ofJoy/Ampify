ae6ce95b3ce1c4e9048ed642fb2f2b6605542d46
Spirit-ofJoy <yashendu.pandey02@gmail.com>
2020-12-13 13:08:02 +0530

    Constant Optimize

--- Server/src/Requests/Request.java
+++ Server/src/Requests/Request.java
@@ -7,5 +7,5 @@ import java.io.Serializable;
 //Object sent from client to Server
 public abstract class Request implements Serializable{
     protected Constant reqType;
-    public abstract Constant getReqType() ;
+    public abstract Constant getReqType();
 }
